294b3159a9dcd99e3722dcbda67d0364ae10b08d
zameerSheikh <zameer@gmail.com>
2020-01-16 16:24:38 +0530

    better design

--- public/script.js
+++ public/script.js
@@ -7,30 +7,31 @@ const messageInput = document.querySelector('.message-input');
 
 
 const name = prompt('what is your name?');
-appendMsg('You joined');
+appendMsg('You joined', true);
 socket.emit('new-user', name);
 socket.on('chat-message', data => {
-    appendMsg(`${data.name}: ${data.message}`)
+    appendMsg(`${data.name}: ${data.message}`, false)
 })
 
 socket.on('user-connected', name => {
-    appendMsg(`${name} joined`);
+    appendMsg(`${name} joined`, false);
 })
 
 socket.on('user-disconnected', name => {
-    appendMsg(`${name} left`);
+    appendMsg(`${name} left`, false);
 })
 
 messageForm.addEventListener('submit', e => {
     e.preventDefault();
     const message = messageInput.value;
-    appendMsg(`You: ${message}`)
+    appendMsg(`You: ${message}`, true)
     socket.emit('send-chat-msg', message);
     messageInput.value = '';
 })
 
-function appendMsg(message){
+function appendMsg(message, isoutgoing){
     msgElement = document.createElement('div');
+    isoutgoing && msgElement.classList.add('outgoing');
     msgElement.innerText = message;
     messageContainer.append(msgElement);
 }
